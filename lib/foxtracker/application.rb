6bb01d38473eb38111d60bc36d16f0daaae01feb
Georg Gadinger <nilsding@nilsding.org>
2018-07-17 22:38:48 +0200

    fix instrument reading and remove :data from inspect output

--- lib/foxtracker/application.rb
+++ lib/foxtracker/application.rb
@@ -21,6 +21,7 @@ module Foxtracker
 
   class Types
     include Dry::Types.module
+    
   end
 
   class ExtendedModule < Dry::Struct
@@ -78,10 +79,10 @@ module Foxtracker
       attribute :type, Types::Strict::Integer
       attribute :panning, Types::Strict::Integer
       attribute :relative_note_number, Types::Strict::Integer
-      attribute :packing_type, Types::Strict::String.enum("delta" => 0, "adpcm" => 0xAD)
+      attribute :packing_type, Types::Strict::Integer # Types::Strict::String.enum("delta" => 0, "adpcm" => 0xAD)
       attribute :name, Types::Strict::String
 
-      attribute :raw_data, Types::Strict::String
+      # attribute :raw_data, Types::Strict::String
       attribute :data, Types::Strict::Array.of(Types::Strict::Integer)
 
       # returns the sample looping type (:none, :forward, or :pingpong)
@@ -98,16 +99,28 @@ module Foxtracker
       def looping?
         !sample_loop_length.zero?
       end
+
+      # hack to remove :data from inspect output as sample data is really too spammy
+      # https://github.com/dry-rb/dry-struct/blob/cb41a5a03/lib/dry/struct.rb#L178
+      def inspect
+        klass = self.class
+        attrs = klass
+                .attribute_names
+                .reject { |key| key == :data }
+                .map { |key| " #{key}=#{@attributes[key].inspect}" }
+                .join
+        "#<#{klass.name || klass.inspect}#{attrs}>"
+      end
     end
 
     class Instrument < Dry::Struct
-      # first part
+      # first part -- these attributes MUST be set on an instrument
       attribute :header_size, Types::Strict::Integer
       attribute :name, Types::Strict::String
       attribute :type, Types::Strict::Integer
       attribute :number_of_samples, Types::Strict::Integer
 
-      # second part
+      # second part -- those are optional attributes
       attribute :sample_header_size, Types::Strict::Integer.meta(omittable: true)
       attribute :sample_keymap_assignments, Types::Strict::Array.of(Types::Strict::Integer)
         .constrained(size: 96).meta(omittable: true)
@@ -126,8 +139,12 @@ module Foxtracker
       attribute :panning_sustain_point, Types::Strict::Integer.meta(omittable: true)
       attribute :panning_loop_start_point, Types::Strict::Integer.meta(omittable: true)
       attribute :panning_loop_end_point, Types::Strict::Integer.meta(omittable: true)
-      attribute :volume_type, Types::Strict::String.enum("on" => 0, "sustain" => 1, "loop" => 2).meta(omittable: true)
-      attribute :panning_type, Types::Strict::String.enum("on" => 0, "sustain" => 1, "loop" => 2).meta(omittable: true)
+      attribute :volume_type, Types::Strict::String
+        .enum("on" => 0, "sustain" => 1, "loop" => 2, "sustain_and_loop" => 3)
+        .meta(omittable: true)
+      attribute :panning_type, Types::Strict::String
+        .enum("on" => 0, "sustain" => 1, "loop" => 2, "sustain_and_loop" => 3)
+        .meta(omittable: true)
       attribute :vibrato_type, Types::Strict::Integer.meta(omittable: true)
       attribute :vibrato_sweep, Types::Strict::Integer.meta(omittable: true)
       attribute :vibrato_depth, Types::Strict::Integer.meta(omittable: true)
@@ -141,6 +158,7 @@ module Foxtracker
     attribute :instruments, Types::Strict::Array.of(Instrument)
 
     def self.parse(bin)
+      bin = -bin
       args = {}
       offset = 0
 
@@ -149,10 +167,10 @@ module Foxtracker
       ##########
       raise "not an XM module" unless bin[offset...(offset += 17)].casecmp("Extended module: ").zero?
 
-      args[:title] = bin[offset...(offset += 20)].strip
+      args[:title] = bin[offset...(offset += 20)].rstrip
       raise "invalid XM module" unless bin[offset...(offset += 1)] == "\x1A"
 
-      args[:tracker] = bin[offset...(offset += 20)].strip
+      args[:tracker] = bin[offset...(offset += 20)].rstrip
 
       args[:version_number] = bin[offset...(offset += 2)].unpack1("S<")
 
@@ -205,16 +223,22 @@ module Foxtracker
       xm_args[:number_of_instruments].times do
         instruments << {}.tap do |instrument_args|
           # 1st part
-          instrument_args[:header_size] =
-            bin[offset...(offset += 2)].unpack1("S<") << 8 | bin[offset...(offset += 2)].unpack1("S<")
-          instrument_args[:name] = bin[offset...(offset += 22)].strip
+          instrument_args[:header_size] = bin[offset...(offset += 4)].unpack1("L<")
+          instrument_args[:name] = bin[offset...(offset += 22)].rstrip
           instrument_args[:type] = bin[offset...(offset += 1)].unpack1("C")
           instrument_args[:number_of_samples] = bin[offset...(offset += 2)].unpack1("S<")
-          next if instrument_args[:number_of_samples].zero?
+
+          puts "instrument #{instrument_args[:name].inspect}:"
+          puts "- header_size: #{format('%#06x', instrument_args[:header_size])}"
+          puts "- number_of_samples: #{instrument_args[:number_of_samples]}"
+          if instrument_args[:number_of_samples].zero?
+            offset -= 33 # realignment hack from xmp
+            next
+          end
 
           # 2nd part
-          instrument_args[:sample_header_size] =
-            bin[offset...(offset += 2)].unpack1("S<") << 8 | bin[offset...(offset += 2)].unpack1("S<")
+          instrument_args[:sample_header_size] = bin[offset...(offset += 4)].unpack1("L<")
+          puts "- sample_header_size: #{format('%#06x', instrument_args[:sample_header_size])}"
           instrument_args[:sample_keymap_assignments] = bin[offset...(offset += 96)].unpack("C*")
           instrument_args[:volume_envelope] = bin[offset...(offset += 48)].unpack("S<*")
           instrument_args[:panning_envelope] = bin[offset...(offset += 48)].unpack("S<*")
@@ -222,14 +246,17 @@ module Foxtracker
           %i[number_of_volume_points number_of_panning_points
              volume_sustain_point volume_loop_start_point volume_loop_end_point
              panning_sustain_point panning_loop_start_point panning_loop_end_point
+             volume_type panning_type
              vibrato_type vibrato_sweep vibrato_depth vibrato_rate].each do |attr|
             instrument_args[attr] = bin[offset...(offset += 1)].unpack1("C")
+            print "- #{attr}: #{format('%#04x', instrument_args[attr])} -- "
+            p instrument_args[attr]
           end
 
           instrument_args[:volume_fadeout] = bin[offset...(offset += 2)].unpack1("S<")
           instrument_args[:reserved] = bin[offset...(offset += 22)].unpack("S<*")
+          puts "- volume_fadeout: #{format('%#06x', instrument_args[:volume_fadeout])}"
 
-          puts "instrument #{instrument_args[:name].inspect}:"
           offset, instrument_args[:samples] = parse_samples(bin, instrument_args, offset)
         end
       end
@@ -240,14 +267,14 @@ module Foxtracker
     def self.parse_samples(bin, instrument_args, offset)
       samples = []
 
+      # first the sample headers ...
+      puts "- samples:"
       instrument_args[:number_of_samples].times do
         samples << {}.tap do |sample_args|
-          sample_args[:sample_length] =
-            bin[offset...(offset += 2)].unpack1("S<") << 8 | bin[offset...(offset += 2)].unpack1("S<")
-          sample_args[:sample_loop_start] =
-            bin[offset...(offset += 2)].unpack1("S<") << 8 | bin[offset...(offset += 2)].unpack1("S<")
-          sample_args[:sample_loop_length] =
-            bin[offset...(offset += 2)].unpack1("S<") << 8 | bin[offset...(offset += 2)].unpack1("S<")
+          start_offset = offset
+          sample_args[:sample_length] = bin[offset...(offset += 4)].unpack1("L<")
+          sample_args[:sample_loop_start] = bin[offset...(offset += 4)].unpack1("L<")
+          sample_args[:sample_loop_length] = bin[offset...(offset += 4)].unpack1("L<")
 
           sample_args[:volume] = bin[offset...(offset += 1)].unpack1("C")
           sample_args[:finetune] = bin[offset...(offset += 1)].unpack1("c")
@@ -256,30 +283,39 @@ module Foxtracker
           sample_args[:relative_note_number] = bin[offset...(offset += 1)].unpack1("c")
           sample_args[:packing_type] = bin[offset...(offset += 1)].unpack1("C")
 
-          sample_args[:name] = bin[offset...(offset += 22)].strip
-          sample_args[:raw_data] = bin[offset...(offset += sample_args[:sample_length])]
-          puts "- sample #{sample_args[:name].inspect}:"
-          puts "  - length: #{format('%#06x', sample_args[:sample_length])}"
-          puts "  - loop_start: #{format('%#06x', sample_args[:sample_loop_start])}"
-          puts "  - loop_length: #{format('%#06x', sample_args[:sample_loop_length])}"
-          puts "  - volume: #{format('%#04x', sample_args[:volume])}"
-          puts "  - finetune: #{format('%#04x', sample_args[:finetune])}"
-          puts "  - panning: #{format('%#04x', sample_args[:panning])}"
-          sample_args[:data] = unpack_sample_data(sample_args)
+          sample_args[:name] = bin[offset...(offset += 22)].rstrip
+          puts "  - name: #{sample_args[:name].inspect}"
+          puts "    type: #{ExtendedModule::Helpers.sample_type(sample_args[:type])}bit"
+          puts "    length: #{format('%#06x', sample_args[:sample_length])}"
+          puts "    loop_start: #{format('%#06x', sample_args[:sample_loop_start])}"
+          puts "    loop_length: #{format('%#06x', sample_args[:sample_loop_length])}"
+          puts "    volume: #{format('%#04x', sample_args[:volume])}"
+          puts "    finetune: #{format('%#04x', sample_args[:finetune])}"
+          puts "    panning: #{format('%#04x', sample_args[:panning])}"
+          diff = instrument_args[:sample_header_size] - (offset - start_offset)
+          puts "    offset diff from instrument header: #{diff}"
+          raise unless diff.zero?
         end
+      end
+
+      # ... then the sample data!
+      samples.each do |sample_args|
+        sample_args[:raw_data] = bin[offset...(offset += sample_args[:sample_length])]
+        sample_args[:data] = unpack_sample_data(sample_args)
       end
 
       [offset, samples]
     end
 
     def self.unpack_sample_data(sample_args)
+      puts "unpacking sample #{sample_args[:name].inspect} ..."
       unpack_str = case ExtendedModule::Helpers.sample_type(sample_args[:type])
                    when 8 then "c*"
                    when 16 then "s<*"
                    else
                      raise "this should never happen(tm)"
                    end
-      packed_samples = sample_args[:raw_data].unpack(unpack_str)
+      packed_samples = sample_args.delete(:raw_data).unpack(unpack_str)
 
       raise "ADPCM samples are not supported yet" if sample_args[:packing_type] == 0xAD
 
